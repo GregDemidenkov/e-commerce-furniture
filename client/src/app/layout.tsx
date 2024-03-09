c91c2f87f2d86cb7d2f4bf8c3a19e8acbcd5dff2
podlunoy <greg.demidenkov@yandex.ru>
2024-03-09 20:25:44 +0300

    Init client project

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "E-commerce Forniture",
+  description: "E-commerce Forniture",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
